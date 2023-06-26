1538cce918f6f07ef3950d145a6900ebe8325025
RestartFU <zerkoming@gmail.com>
2023-06-26 16:29:38 -0700

    implement rogue

--- moyai/user/handler.go
+++ moyai/user/handler.go
@@ -8,6 +8,9 @@ import (
 	"github.com/df-mc/dragonfly/server/player"
 	"github.com/df-mc/dragonfly/server/session"
 	"github.com/df-mc/dragonfly/server/world"
+	"github.com/df-mc/dragonfly/server/world/particle"
+	"github.com/df-mc/dragonfly/server/world/sound"
+	"github.com/go-gl/mathgl/mgl64"
 	"github.com/moyai-network/moose"
 	"github.com/moyai-network/moose/class"
 	"github.com/moyai-network/moose/lang"
@@ -38,6 +41,7 @@ type Handler struct {
 	p *player.Player
 
 	pearlCooldown *moose.CoolDown
+	rogueCooldown *moose.CoolDown
 
 	class      atomic.Value[moose.Class]
 	bardEnergy atomic.Value[float64]
@@ -51,6 +55,7 @@ func NewHandler(p *player.Player) *Handler {
 		p: p,
 
 		pearlCooldown: moose.NewCoolDown(),
+		rogueCooldown: moose.NewCoolDown(),
 
 		combatTag: moose.NewTag(nil, nil),
 		archerTag: moose.NewTag(nil, nil),
@@ -106,6 +111,7 @@ func (h *Handler) HandleHurt(ctx *event.Context, dmg *float64, _ *time.Duration,
 			return
 		}
 		if ha := target.Handler().(*Handler); !class.Compare(h.class.Load(), class.Archer{}) && class.Compare(ha.class.Load(), class.Archer{}) && (s.Projectile.Type() == (entity.ArrowType{})) {
+			ctx.Cancel()
 			ha.archerTag.Set(time.Second * 10)
 
 			dist := h.p.Position().Sub(target.Position()).Len()
@@ -114,6 +120,10 @@ func (h *Handler) HandleHurt(ctx *event.Context, dmg *float64, _ *time.Duration,
 				d = 20
 			}
 			*dmg = (d / 10) * 2
+			h.p.Hurt(*dmg, NoArmourAttackEntitySource{
+				Attacker: h.p,
+			})
+			h.p.KnockBack(target.Position(), 0.394, 0.394)
 
 			target.Message(lang.Translatef(h.p.Locale(), "archer.tag", math.Round(dist), *dmg/2))
 		}
@@ -146,6 +156,30 @@ func (h *Handler) HandleAttackEntity(ctx *event.Context, e world.Entity, force, 
 		return
 	}
 
+	held, left := h.p.HeldItems()
+	if s, ok := held.Item().(item.Sword); ok && s.Tier == item.ToolTierGold && class.Compare(h.class.Load(), class.Rogue{}) && t.Rotation().Direction() == h.p.Rotation().Direction() {
+		cd := h.rogueCooldown
+		w := h.p.World()
+		if cd.Active() {
+			h.p.Message(lang.Translatef(h.p.Locale(), "user.cool-down", "Rogue", cd.Remaining().Seconds()))
+		} else {
+			ctx.Cancel()
+			for i := 1; i <= 3; i++ {
+				w.AddParticle(t.Position().Add(mgl64.Vec3{0, float64(i), 0}), particle.Dust{
+					Colour: item.ColourRed().RGBA(),
+				})
+			}
+			w.PlaySound(h.p.Position(), sound.ItemBreak{})
+			t.Hurt(8, NoArmourAttackEntitySource{
+				Attacker: h.p,
+			})
+			t.KnockBack(h.p.Position(), *force, *height)
+
+			h.p.SetHeldItems(item.Stack{}, left)
+			cd.Set(time.Second * 10)
+		}
+	}
+
 	t.Handler().(*Handler).combatTag.Set(time.Second * 20)
 	h.combatTag.Set(time.Second * 20)
 }
@@ -158,4 +192,32 @@ func (h *Handler) HandleQuit() {
 	playersMu.Lock()
 	delete(players, h.p.XUID())
 	playersMu.Unlock()
+}
+
+type NoArmourAttackEntitySource struct {
+	Attacker world.Entity
+}
+
+func (NoArmourAttackEntitySource) Fire() bool {
+	return false
+}
+
+func (NoArmourAttackEntitySource) ReducedByArmour() bool {
+	return false
+}
+
+func (NoArmourAttackEntitySource) ReducedByResistance() bool {
+	return false
+}
+
+// attackerFromSource returns the Attacker from a DamageSource. If the source is not an entity false is
+// returned.
+func attackerFromSource(src world.DamageSource) (world.Entity, bool) {
+	switch s := src.(type) {
+	case entity.AttackDamageSource:
+		return s.Attacker, true
+	case NoArmourAttackEntitySource:
+		return s.Attacker, true
+	}
+	return nil, false
 }
