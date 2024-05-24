2d9c936bf8abc7e471394090725c9137baed7440
Restart <45609733+RestartFU@users.noreply.github.com>
2024-05-24 14:58:57 -0400

    speed

--- moyai/kit/diamond.go
+++ moyai/kit/diamond.go
@@ -2,6 +2,7 @@ package kit
 
 import (
 	"github.com/df-mc/dragonfly/server/item"
+	"github.com/df-mc/dragonfly/server/item/enchantment"
 	"github.com/df-mc/dragonfly/server/item/potion"
 	"github.com/df-mc/dragonfly/server/player"
 	ench "github.com/moyai-network/teams/moyai/enchantment"
@@ -49,10 +50,43 @@ func (d Diamond) Items(*player.Player) [36]item.Stack {
 
 // Armour ...
 func (d Diamond) Armour(*player.Player) [4]item.Stack {
-	return armour(d.Free, [4]item.ArmourTier{
-		item.ArmourTierDiamond{},
-		item.ArmourTierDiamond{},
-		item.ArmourTierDiamond{},
-		item.ArmourTierDiamond{},
-	})
+	var lvl = 2
+	if d.Free {
+		lvl = 1
+	}
+
+	protection := item.NewEnchantment(ench.Protection{}, lvl)
+	unbreaking := item.NewEnchantment(enchantment.Unbreaking{}, 3)
+
+	invis := item.NewEnchantment(ench.Invisibility{}, 1)
+	nightVision := item.NewEnchantment(ench.NightVision{}, 1)
+	fireRes := item.NewEnchantment(ench.FireResistance{}, 1)
+	recovery := item.NewEnchantment(ench.Recovery{}, 1)
+
+	var (
+		defaultEnchants = []item.Enchantment{
+			protection,
+			unbreaking,
+		}
+
+		helmetEnchants     = defaultEnchants
+		chestplateEnchants = defaultEnchants
+		leggingsEnchants   = defaultEnchants
+		bootsEnchants      = defaultEnchants
+	)
+
+	if !d.Free {
+		helmetEnchants = append(helmetEnchants, invis, nightVision)
+		chestplateEnchants = append(chestplateEnchants, fireRes)
+		leggingsEnchants = append(leggingsEnchants, recovery)
+		bootsEnchants = append(bootsEnchants, item.NewEnchantment(enchantment.FeatherFalling{}, 4))
+		bootsEnchants = append(bootsEnchants, item.NewEnchantment(ench.Speed{}, 2))
+	}
+
+	return [4]item.Stack{
+		item.NewStack(item.Helmet{Tier: item.ArmourTierDiamond{}}, 1).WithEnchantments(helmetEnchants...),
+		item.NewStack(item.Chestplate{Tier: item.ArmourTierDiamond{}}, 1).WithEnchantments(chestplateEnchants...),
+		item.NewStack(item.Leggings{Tier: item.ArmourTierDiamond{}}, 1).WithEnchantments(leggingsEnchants...),
+		item.NewStack(item.Boots{Tier: item.ArmourTierDiamond{}}, 1).WithEnchantments(bootsEnchants...),
+	}
 }
