d7d51ee6c98b6bb822bc6c893fc29c8c32ec861b
Restart <45609733+RestartFU@users.noreply.github.com>
2024-05-26 17:16:16 -0400

    Update stop.go

--- moyai/command/stop.go
+++ moyai/command/stop.go
@@ -5,6 +5,7 @@ import (
 	"github.com/moyai-network/teams/moyai"
 	"github.com/moyai-network/teams/moyai/data"
 	"github.com/moyai-network/teams/moyai/role"
+	"os"
 	"time"
 )
 
@@ -14,6 +15,7 @@ func (Stop) Run(src cmd.Source, out *cmd.Output) {
 	time.Sleep(time.Millisecond * 500)
 	data.FlushCache()
 	_ = moyai.Server().Close()
+	os.Exit(0)
 }
 
 func (Stop) Allow(src cmd.Source) bool {
