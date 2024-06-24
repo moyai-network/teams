6efc9e2f898d7a09dece99e04393974896cf946a
cqdetdisc <cqdetdisc@gmail.com>
2024-06-24 10:07:53 -0400

    anglais

--- cmd/discord/command/link.go
+++ cmd/discord/command/link.go
@@ -3,11 +3,12 @@ package command
 import (
 	"context"
 	"fmt"
+	"strings"
+
 	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/api/cmdroute"
 	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/moyai-network/teams/moyai/data"
-	"strings"
 )
 
 func (h *Handler) link(ctx context.Context, d cmdroute.CommandData) *api.InteractionResponseData {
@@ -17,5 +18,5 @@ func (h *Handler) link(ctx context.Context, d cmdroute.CommandData) *api.Interac
 	}
 	_ = h.s.AddRole(h.guildID, d.Event.Sender().ID, discord.RoleID(1209213713337548830), api.AddRoleData{AuditLogReason: "Linking"})
 	_ = h.s.RemoveRole(h.guildID, d.Event.Sender().ID, discord.RoleID(1198584931459272806), "Linking")
-	return h.success(fmt.Sprintf("Votre compte Minecraft **%s** est désormais lié à votre compte Discord !", strings.ReplaceAll(u.DisplayName, "_", " ")))
+	return h.success(fmt.Sprintf("Your MC Account (**%s**) has been linked to the Discord!", strings.ReplaceAll(u.DisplayName, "_", " ")))
 }
