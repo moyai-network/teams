c02ad7f2a89393eb3e2243efceac40acd23c499b
Restart <45609733+RestartFU@users.noreply.github.com>
2024-06-25 19:14:25 -0400

    Update allower.go

--- moyai/allower.go
+++ moyai/allower.go
@@ -1,6 +1,7 @@
 package moyai
 
 import (
+	"fmt"
 	"net"
 	"strings"
 
@@ -25,6 +26,7 @@ func NewAllower(whitelisted bool) *Allower {
 func (a *Allower) Allow(addr net.Addr, d login.IdentityData, c login.ClientData) (string, bool) {
 	u, err := data.LoadUserOrCreate(d.DisplayName, d.XUID)
 	if err != nil {
+		fmt.Printf("Failed to load user: %v\n", err)
 		return lang.Translatef(data.Language{}, "user.data.load.error"), false
 	}
 	if a.whitelisted && !u.Whitelisted {
