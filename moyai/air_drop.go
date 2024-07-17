f7b78467a2d62805db8b7250431b0afe270f492a
Restart <45609733+RestartFU@users.noreply.github.com>
2024-07-17 03:58:35 -0400

    oh lee sheet

--- moyai/air_drop.go
+++ moyai/air_drop.go
@@ -1,254 +1,254 @@
 package moyai
 
 import (
-	_ "embed"
-	"math/rand"
-	"time"
+    _ "embed"
+    "math/rand"
+    "time"
 
-	"github.com/df-mc/dragonfly/server/block"
-	"github.com/df-mc/dragonfly/server/block/cube"
-	"github.com/df-mc/dragonfly/server/event"
-	"github.com/df-mc/dragonfly/server/item"
-	"github.com/df-mc/dragonfly/server/item/inventory"
-	"github.com/df-mc/dragonfly/server/world"
-	"github.com/df-mc/dragonfly/server/world/sound"
-	"github.com/moyai-network/teams/moyai/area"
-	it "github.com/moyai-network/teams/moyai/item"
-	"github.com/restartfu/gophig"
-	"github.com/sandertv/gophertunnel/minecraft/text"
+    "github.com/df-mc/dragonfly/server/block"
+    "github.com/df-mc/dragonfly/server/block/cube"
+    "github.com/df-mc/dragonfly/server/event"
+    "github.com/df-mc/dragonfly/server/item"
+    "github.com/df-mc/dragonfly/server/item/inventory"
+    "github.com/df-mc/dragonfly/server/world"
+    "github.com/df-mc/dragonfly/server/world/sound"
+    "github.com/moyai-network/teams/moyai/area"
+    it "github.com/moyai-network/teams/moyai/item"
+    "github.com/restartfu/gophig"
+    "github.com/sandertv/gophertunnel/minecraft/text"
 )
 
 func checkErr(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
 
 func init() {
-	err := gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_1.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_1.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_2.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets2)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_2.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets2)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_3.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets3)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_3.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets3)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_4.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets4)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_4.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets4)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_5.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets5)
-	checkErr(err)
-	err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_5.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets5)
-	checkErr(err)
+    err := gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_1.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_1.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_2.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets2)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_2.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets2)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_3.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets3)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_3.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets3)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_4.json", gophig.JSONMarshaler{}, &parachuteFenceOffsets4)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_4.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets4)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_fence_5.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets5)
+    checkErr(err)
+    err = gophig.GetConfComplex("assets/air_drop/parachute_offset_wool_5.json", gophig.JSONMarshaler{}, &parachuteWoolOffsets5)
+    checkErr(err)
 }
 
 var (
-	lastDropPos cube.Pos
+    lastDropPos cube.Pos
 
-	parachuteFenceOffsets  []cube.Pos
-	parachuteWoolOffsets   []cube.Pos
-	parachuteFenceOffsets2 []cube.Pos
-	parachuteFenceOffsets3 []cube.Pos
-	parachuteFenceOffsets4 []cube.Pos
-	parachuteFenceOffsets5 []cube.Pos
-	parachuteWoolOffsets2  []cube.Pos
-	parachuteWoolOffsets3  []cube.Pos
-	parachuteWoolOffsets4  []cube.Pos
-	parachuteWoolOffsets5  []cube.Pos
+    parachuteFenceOffsets  []cube.Pos
+    parachuteWoolOffsets   []cube.Pos
+    parachuteFenceOffsets2 []cube.Pos
+    parachuteFenceOffsets3 []cube.Pos
+    parachuteFenceOffsets4 []cube.Pos
+    parachuteFenceOffsets5 []cube.Pos
+    parachuteWoolOffsets2  []cube.Pos
+    parachuteWoolOffsets3  []cube.Pos
+    parachuteWoolOffsets4  []cube.Pos
+    parachuteWoolOffsets5  []cube.Pos
 )
 
 func tickAirDrop(w *world.World) {
-	for {
-		<-time.After(time.Hour)
-		pos := findAirDropPosition(w)
-		destroyAirDrop(w, lastDropPos)
-		dropAirDrop(w, pos)
-		return
-	}
+    for {
+        <-time.After(time.Hour)
+        pos := findAirDropPosition(w)
+        destroyAirDrop(w, lastDropPos)
+        dropAirDrop(w, pos)
+        return
+    }
 }
 
 func dropAirDrop(w *world.World, pos cube.Pos) {
-	Broadcastf("airdrop.incoming", pos.X(), pos.Z())
+    Broadcastf("airdrop.incoming", pos.X(), pos.Z())
 
-	bl := generateAirDrop(w)
-	w.SetBlock(pos, bl, nil)
-	for _, p := range Players() {
-		p.PlaySound(sound.BarrelClose{})
-		p.PlaySound(sound.FireworkBlast{})
-		p.PlaySound(sound.FireworkTwinkle{})
-		p.PlaySound(sound.Note{})
-	}
+    bl := generateAirDrop(w)
+    w.SetBlock(pos, bl, nil)
+    for _, p := range Players() {
+        p.PlaySound(sound.BarrelClose{})
+        p.PlaySound(sound.FireworkBlast{})
+        p.PlaySound(sound.FireworkTwinkle{})
+        p.PlaySound(sound.Note{})
+    }
 
-	for {
-		<-time.After(time.Second)
+    for {
+        <-time.After(time.Second)
 
-		oldPos := pos
-		pos = pos.Add(cube.Pos{0, -1, 0})
-		if _, ok := w.Block(pos).(block.Air); !ok {
-			removeParachute(w, oldPos)
-			placeParachuteBlock(parachuteFenceOffsets2, w, pos, block.WoodFence{Wood: block.OakWood()})
-			placeParachuteBlock(parachuteWoolOffsets2, w, pos, block.Wool{Colour: item.ColourRed()})
-			<-time.After(time.Second / 4)
-			removeParachuteBlock(parachuteFenceOffsets2, w, pos)
-			removeParachuteBlock(parachuteWoolOffsets2, w, pos)
-			placeParachuteBlock(parachuteFenceOffsets3, w, pos, block.WoodFence{Wood: block.OakWood()})
-			placeParachuteBlock(parachuteWoolOffsets3, w, pos, block.Wool{Colour: item.ColourRed()})
-			<-time.After(time.Second / 4)
-			removeParachuteBlock(parachuteFenceOffsets3, w, pos)
-			removeParachuteBlock(parachuteWoolOffsets3, w, pos)
-			placeParachuteBlock(parachuteFenceOffsets4, w, pos, block.WoodFence{Wood: block.OakWood()})
-			placeParachuteBlock(parachuteWoolOffsets4, w, pos, block.Wool{Colour: item.ColourRed()})
-			<-time.After(time.Second / 4)
-			removeParachuteBlock(parachuteFenceOffsets4, w, pos)
-			removeParachuteBlock(parachuteWoolOffsets4, w, pos)
-			placeParachuteBlock(parachuteFenceOffsets5, w, pos, block.WoodFence{Wood: block.OakWood()})
-			placeParachuteBlock(parachuteWoolOffsets5, w, pos, block.Wool{Colour: item.ColourRed()})
-			<-time.After(time.Second / 4)
-			removeParachuteBlock(parachuteFenceOffsets5, w, pos)
-			removeParachuteBlock(parachuteWoolOffsets5, w, pos)
-			fillAirDrop(bl.Inventory())
+        oldPos := pos
+        pos = pos.Add(cube.Pos{0, -1, 0})
+        if _, ok := w.Block(pos).(block.Air); !ok {
+            removeParachute(w, oldPos)
+            placeParachuteBlock(parachuteFenceOffsets2, w, pos, block.WoodFence{Wood: block.OakWood()})
+            placeParachuteBlock(parachuteWoolOffsets2, w, pos, block.Wool{Colour: item.ColourRed()})
+            <-time.After(time.Second / 4)
+            removeParachuteBlock(parachuteFenceOffsets2, w, pos)
+            removeParachuteBlock(parachuteWoolOffsets2, w, pos)
+            placeParachuteBlock(parachuteFenceOffsets3, w, pos, block.WoodFence{Wood: block.OakWood()})
+            placeParachuteBlock(parachuteWoolOffsets3, w, pos, block.Wool{Colour: item.ColourRed()})
+            <-time.After(time.Second / 4)
+            removeParachuteBlock(parachuteFenceOffsets3, w, pos)
+            removeParachuteBlock(parachuteWoolOffsets3, w, pos)
+            placeParachuteBlock(parachuteFenceOffsets4, w, pos, block.WoodFence{Wood: block.OakWood()})
+            placeParachuteBlock(parachuteWoolOffsets4, w, pos, block.Wool{Colour: item.ColourRed()})
+            <-time.After(time.Second / 4)
+            removeParachuteBlock(parachuteFenceOffsets4, w, pos)
+            removeParachuteBlock(parachuteWoolOffsets4, w, pos)
+            placeParachuteBlock(parachuteFenceOffsets5, w, pos, block.WoodFence{Wood: block.OakWood()})
+            placeParachuteBlock(parachuteWoolOffsets5, w, pos, block.Wool{Colour: item.ColourRed()})
+            <-time.After(time.Second / 4)
+            removeParachuteBlock(parachuteFenceOffsets5, w, pos)
+            removeParachuteBlock(parachuteWoolOffsets5, w, pos)
+            fillAirDrop(bl.Inventory())
 
-			ch, ok := w.Block(oldPos).(block.Chest)
-			if !ok {
-				break
-			}
-			h, ok := ch.Inventory().Handler().(*airDropInventoryHandler)
-			if !ok {
-				break
-			}
-			h.pos = oldPos
+            ch, ok := w.Block(oldPos).(block.Chest)
+            if !ok {
+                break
+            }
+            h, ok := ch.Inventory().Handler().(*airDropInventoryHandler)
+            if !ok {
+                break
+            }
+            h.pos = oldPos
 
-			for _, p := range w.Viewers(pos.Vec3()) {
-				p.ViewSound(pos.Vec3Centre(), sound.Fall{})
-			}
+            for _, p := range w.Viewers(pos.Vec3()) {
+                p.ViewSound(pos.Vec3Centre(), sound.Fall{})
+            }
 
-			lastDropPos = oldPos
-			return
-		}
-		removeParachute(w, oldPos)
-		w.SetBlock(oldPos, block.Air{}, nil)
+            lastDropPos = oldPos
+            return
+        }
+        removeParachute(w, oldPos)
+        w.SetBlock(oldPos, block.Air{}, nil)
 
-		placeParachute(w, pos)
-		w.SetBlock(pos, bl, nil)
-	}
+        placeParachute(w, pos)
+        w.SetBlock(pos, bl, nil)
+    }
 }
 
 func placeParachute(w *world.World, pos cube.Pos) {
-	placeParachuteBlock(parachuteFenceOffsets, w, pos, block.WoodFence{Wood: block.OakWood()})
-	placeParachuteBlock(parachuteWoolOffsets, w, pos, block.Wool{Colour: item.ColourRed()})
+    placeParachuteBlock(parachuteFenceOffsets, w, pos, block.WoodFence{Wood: block.OakWood()})
+    placeParachuteBlock(parachuteWoolOffsets, w, pos, block.Wool{Colour: item.ColourRed()})
 }
 
 func removeParachute(w *world.World, pos cube.Pos) {
-	removeParachuteBlock(parachuteFenceOffsets, w, pos)
-	removeParachuteBlock(parachuteWoolOffsets, w, pos)
+    removeParachuteBlock(parachuteFenceOffsets, w, pos)
+    removeParachuteBlock(parachuteWoolOffsets, w, pos)
 }
 
 func placeParachuteBlock(offsets []cube.Pos, w *world.World, pos cube.Pos, bl world.Block) {
-	for _, off := range offsets {
-		newPos := pos.Add(off)
-		if _, ok := w.Block(newPos).(block.Air); !ok {
-			continue
-		}
-		w.SetBlock(newPos, bl, nil)
-	}
+    for _, off := range offsets {
+        newPos := pos.Add(off)
+        if _, ok := w.Block(newPos).(block.Air); !ok {
+            continue
+        }
+        w.SetBlock(newPos, bl, nil)
+    }
 }
 
 func removeParachuteBlock(offsets []cube.Pos, w *world.World, pos cube.Pos) {
-	for _, off := range offsets {
-		newPos := pos.Add(off)
-		_, fence := w.Block(newPos).(block.WoodFence)
-		if _, wool := w.Block(newPos).(block.Wool); !fence && !wool {
-			continue
-		}
-		w.SetBlock(newPos, block.Air{}, nil)
-	}
+    for _, off := range offsets {
+        newPos := pos.Add(off)
+        _, fence := w.Block(newPos).(block.WoodFence)
+        if _, wool := w.Block(newPos).(block.Wool); !fence && !wool {
+            continue
+        }
+        w.SetBlock(newPos, block.Air{}, nil)
+    }
 }
 
 func generateAirDrop(w *world.World) block.Chest {
-	bl := block.NewChest()
-	bl = bl.WithName(text.Colourf("<red>Air Drop</red>")).(block.Chest)
-	bl.Inventory().Handle(&airDropInventoryHandler{
-		inv: bl.Inventory(),
-		w:   w,
-	})
-	return bl
+    bl := block.NewChest()
+    bl = bl.WithName(text.Colourf("<red>Air Drop</red>")).(block.Chest)
+    bl.Inventory().Handle(&airDropInventoryHandler{
+        inv: bl.Inventory(),
+        w:   w,
+    })
+    return bl
 }
 
 func fillAirDrop(inventory *inventory.Inventory) {
-	for i := 0; i < 27; i++ {
-		if rand.Intn(2) == 0 {
-			_ = inventory.SetItem(i, randomItem())
-		}
-	}
+    for i := 0; i < 27; i++ {
+        if rand.Intn(2) == 0 {
+            _ = inventory.SetItem(i, randomItem())
+        }
+    }
 }
 
 func randomItem() item.Stack {
-	items := it.PartnerItems()
-	return it.NewSpecialItem(items[rand.Intn(len(items))], rand.Intn(5))
+    items := it.PartnerItems()
+    return it.NewSpecialItem(items[rand.Intn(len(items))], rand.Intn(5))
 }
 
 func findAirDropPosition(w *world.World) cube.Pos {
-	spawn := area.Spawn(w)
-	warzone := area.WarZone(w)
+    spawn := area.Spawn(w)
+    warzone := area.WarZone(w)
 
-	for {
-		x := warzone.Min().X() + (warzone.Max().X()-warzone.Min().X())*rand.Float64()
-		z := warzone.Min().Y() + (warzone.Max().Y()-warzone.Min().Y())*rand.Float64()
-		pos := cube.Pos{int(x), 255, int(z)}
-		roads := area.Roads(w)
-		valid := true
-		for _, r := range roads {
-			if r.Vec3WithinOrEqualFloorXZ(pos.Vec3Centre()) {
-				valid = false
-			}
-		}
-		if !spawn.Vec3WithinOrEqualFloorXZ(pos.Vec3Centre()) && valid {
-			return pos
-		}
-	}
+    for {
+        x := warzone.Min().X() + (warzone.Max().X()-warzone.Min().X())*rand.Float64()
+        z := warzone.Min().Y() + (warzone.Max().Y()-warzone.Min().Y())*rand.Float64()
+        pos := cube.Pos{int(x), 255, int(z)}
+        roads := area.Roads(w)
+        valid := true
+        for _, r := range roads {
+            if r.Vec3WithinOrEqualFloorXZ(pos.Vec3Centre()) {
+                valid = false
+            }
+        }
+        if !spawn.Vec3WithinOrEqualFloorXZ(pos.Vec3Centre()) && valid {
+            return pos
+        }
+    }
 }
 
 type airDropInventoryHandler struct {
-	inventory.NopHandler
-	inv *inventory.Inventory
-	pos cube.Pos
-	w   *world.World
+    inventory.NopHandler
+    inv *inventory.Inventory
+    pos cube.Pos
+    w   *world.World
 }
 
 func (h airDropInventoryHandler) HandleTake(_ *event.Context, _ int, st item.Stack) {
-	stacks := h.inv.Items()
-	if len(stacks) == 1 && stacks[0].Equal(st) {
-		time.AfterFunc(time.Second, func() {
-			destroyAirDrop(h.w, h.pos)
-		})
-		return
-	}
+    stacks := h.inv.Items()
+    if len(stacks) == 1 && stacks[0].Equal(st) {
+        time.AfterFunc(time.Second, func() {
+            destroyAirDrop(h.w, h.pos)
+        })
+        return
+    }
 }
 func (airDropInventoryHandler) HandlePlace(ctx *event.Context, _ int, _ item.Stack) {
-	ctx.Cancel()
+    ctx.Cancel()
 }
 
 func (h airDropInventoryHandler) HandleDrop(_ *event.Context, _ int, st item.Stack) {
-	stacks := h.inv.Items()
-	if len(stacks) == 1 && stacks[0].Equal(st) {
-		time.AfterFunc(time.Second, func() {
-			destroyAirDrop(h.w, h.pos)
-		})
-		return
-	}
+    stacks := h.inv.Items()
+    if len(stacks) == 1 && stacks[0].Equal(st) {
+        time.AfterFunc(time.Second, func() {
+            destroyAirDrop(h.w, h.pos)
+        })
+        return
+    }
 }
 
 func destroyAirDrop(w *world.World, pos cube.Pos) {
-	if _, ok := w.Block(pos).(block.Air); ok {
-		return
-	}
-	for _, v := range w.Viewers(pos.Vec3()) {
-		v.ViewSound(pos.Vec3Centre(), sound.Explosion{})
-	}
-	w.SetBlock(pos, block.Air{}, nil)
+    if _, ok := w.Block(pos).(block.Air); ok {
+        return
+    }
+    for _, v := range w.Viewers(pos.Vec3()) {
+        v.ViewSound(pos.Vec3Centre(), sound.Explosion{})
+    }
+    w.SetBlock(pos, block.Air{}, nil)
 }
--- moyai/moyai.go
+++ moyai/moyai.go
@@ -1,254 +1,254 @@
 package moyai
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 
-	"github.com/bedrock-gophers/provider/provider"
-	"github.com/google/uuid"
+    "github.com/bedrock-gophers/provider/provider"
+    "github.com/google/uuid"
 
-	"github.com/diamondburned/arikawa/v3/state"
+    "github.com/diamondburned/arikawa/v3/state"
 
-	"github.com/moyai-network/teams/moyai/data"
+    "github.com/moyai-network/teams/moyai/data"
 
-	"github.com/moyai-network/teams/moyai/sotw"
-	"github.com/sirupsen/logrus"
+    "github.com/moyai-network/teams/moyai/sotw"
+    "github.com/sirupsen/logrus"
 
-	"github.com/df-mc/dragonfly/server/player"
+    "github.com/df-mc/dragonfly/server/player"
 
-	"github.com/df-mc/dragonfly/server"
-	"github.com/df-mc/dragonfly/server/world"
-	"github.com/df-mc/dragonfly/server/world/mcdb"
+    "github.com/df-mc/dragonfly/server"
+    "github.com/df-mc/dragonfly/server/world"
+    "github.com/df-mc/dragonfly/server/world/mcdb"
 )
 
 func init() {
-	go func() {
-		for Overworld() == nil {
-			<-time.After(time.Millisecond)
-			continue
-		}
-		//go tickAirDrop(Overworld())
-		go tickAutomaticSave(Overworld())
-	}()
+    go func() {
+        for Overworld() == nil {
+            <-time.After(time.Millisecond)
+            continue
+        }
+        //go tickAirDrop(Overworld())
+        go tickAutomaticSave(Overworld())
+    }()
 }
 
 var (
-	// discordState is the discord state of the Moyai bot.
-	discordState *state.State
-	// srv is the server instance of the Moyai server.
-	srv *server.Server
-	// playerProvider is the player provider of the Moyai server.
-	playerProvider *provider.Provider
-	// end is the world of the End dimension.
-	end *world.World
-	// nether is the world of the Nether dimension.
-	nether *world.World
-	// deathban is the world of the Deathban arena.
-	deathban *world.World
+    // discordState is the discord state of the Moyai bot.
+    discordState *state.State
+    // srv is the server instance of the Moyai server.
+    srv *server.Server
+    // playerProvider is the player provider of the Moyai server.
+    playerProvider *provider.Provider
+    // end is the world of the End dimension.
+    end *world.World
+    // nether is the world of the Nether dimension.
+    nether *world.World
+    // deathban is the world of the Deathban arena.
+    deathban *world.World
 
-	// lastBlackMarket is the time at which the last black market was opened.
-	lastBlackMarket time.Time
-	// blackMarketOpened is the time at which the black market was last opened.
-	blackMarketOpened time.Time
+    // lastBlackMarket is the time at which the last black market was opened.
+    lastBlackMarket time.Time
+    // blackMarketOpened is the time at which the black market was last opened.
+    blackMarketOpened time.Time
 
-	// chatGameWord is the word to guess in the chat game.
-	chatGameWord string
+    // chatGameWord is the word to guess in the chat game.
+    chatGameWord string
 )
 
 func SetDiscordState(s *state.State) {
-	discordState = s
+    discordState = s
 }
 
 func DiscordState() *state.State {
-	return discordState
+    return discordState
 }
 
 func Worlds() []*world.World {
-	return []*world.World{Overworld(), Nether(), End(), Deathban()}
+    return []*world.World{Overworld(), Nether(), End(), Deathban()}
 }
 
 func Overworld() *world.World {
-	if srv == nil {
-		return nil
-	}
-	return srv.World()
+    if srv == nil {
+        return nil
+    }
+    return srv.World()
 }
 
 func End() *world.World {
-	return end
+    return end
 }
 
 func Nether() *world.World {
-	return nether
+    return nether
 }
 
 func Deathban() *world.World {
-	return deathban
+    return deathban
 }
 
 func Players() []*player.Player {
-	return srv.Players()
+    return srv.Players()
 }
 
 func PlayerProvider() *provider.Provider {
-	return playerProvider
+    return playerProvider
 }
 
 func LoadPlayerData(uuid uuid.UUID) (player.Data, error) {
-	dat, err := playerProvider.Load(uuid, func(dimension world.Dimension) *world.World {
-		switch dimension {
-		case world.Overworld:
-			return Overworld()
-		case world.Nether:
-			return Nether()
-		case world.End:
-			return End()
-		}
-		return nil
-	})
-	return dat, err
+    dat, err := playerProvider.Load(uuid, func(dimension world.Dimension) *world.World {
+        switch dimension {
+        case world.Overworld:
+            return Overworld()
+        case world.Nether:
+            return Nether()
+        case world.End:
+            return End()
+        }
+        return nil
+    })
+    return dat, err
 }
 
 func wrld(dim world.Dimension) *world.World {
-	switch dim {
-	case world.Overworld:
-		return Overworld()
-	case world.Nether:
-		return Nether()
-	case world.End:
-		return End()
-	}
-	return nil
+    switch dim {
+    case world.Overworld:
+        return Overworld()
+    case world.Nether:
+        return Nether()
+    case world.End:
+        return End()
+    }
+    return nil
 }
 
 func NewServer(config server.Config) *server.Server {
-	providerSettings := provider.DefaultSettings()
-	providerSettings.UseServerWorld = false
-	providerSettings.World = wrld
-	providerSettings.FlushRate = time.Minute * 10
-	providerSettings.SaveEffects = false
+    providerSettings := provider.DefaultSettings()
+    providerSettings.UseServerWorld = false
+    providerSettings.World = wrld
+    providerSettings.FlushRate = time.Minute * 10
+    providerSettings.SaveEffects = false
 
-	playerProvider = provider.NewProvider(&config, providerSettings)
-	srv = config.New()
-	return srv
+    playerProvider = provider.NewProvider(&config, providerSettings)
+    srv = config.New()
+    return srv
 }
 
 func LastBlackMarket() time.Time {
-	return lastBlackMarket
+    return lastBlackMarket
 }
 
 func SetLastBlackMarket(t time.Time) {
-	lastBlackMarket = t
+    lastBlackMarket = t
 }
 
 func BlackMarketOpened() time.Time {
-	return blackMarketOpened
+    return blackMarketOpened
 }
 
 func SetBlackMarketOpened(t time.Time) {
-	blackMarketOpened = t
+    blackMarketOpened = t
 }
 
 func ChatGameWord() string {
-	return chatGameWord
+    return chatGameWord
 }
 
 func SetChatGameWord(w string) {
-	chatGameWord = w
+    chatGameWord = w
 }
 
 func ConfigureDimensions(reg world.EntityRegistry, netherFolder, endFolder string) (*world.World, *world.World) {
-	endProv, err := mcdb.Open(endFolder)
-	if err != nil {
-		panic(err)
-	}
-	end = world.Config{
-		Provider: endProv,
-		Dim:      world.End,
-		Entities: reg,
-	}.New()
+    endProv, err := mcdb.Open(endFolder)
+    if err != nil {
+        panic(err)
+    }
+    end = world.Config{
+        Provider: endProv,
+        Dim:      world.End,
+        Entities: reg,
+    }.New()
 
-	netherProv, err := mcdb.Open(netherFolder)
-	if err != nil {
-		panic(err)
-	}
+    netherProv, err := mcdb.Open(netherFolder)
+    if err != nil {
+        panic(err)
+    }
 
-	nether = world.Config{
-		Provider: netherProv,
-		Dim:      world.Nether,
-		Entities: reg,
-	}.New()
+    nether = world.Config{
+        Provider: netherProv,
+        Dim:      world.Nether,
+        Entities: reg,
+    }.New()
 
-	return nether, end
+    return nether, end
 }
 
 func ConfigureDeathban(reg world.EntityRegistry, folder string) *world.World {
-	deathbanProv, err := mcdb.Open(folder)
-	if err != nil {
-		panic(err)
-	}
-	deathban = world.Config{
-		Provider:        deathbanProv,
-		Dim:             world.Overworld,
-		Entities:        reg,
-		RandomTickSpeed: -1,
-	}.New()
+    deathbanProv, err := mcdb.Open(folder)
+    if err != nil {
+        panic(err)
+    }
+    deathban = world.Config{
+        Provider:        deathbanProv,
+        Dim:             world.Overworld,
+        Entities:        reg,
+        RandomTickSpeed: -1,
+    }.New()
 
-	return deathban
+    return deathban
 }
 
 func Close() {
-	data.FlushCache()
-	destroyAirDrop(srv.World(), lastDropPos)
-	for _, p := range Players() {
-		u, err := data.LoadUserFromName(p.Name())
-		if err != nil {
-			continue
-		}
-		h, ok := p.Handler().(userHandler)
-		if !ok {
-			continue
-		}
-		u.PlayTime += h.LogTime()
-		data.SaveUser(u)
+    data.FlushCache()
+    destroyAirDrop(srv.World(), lastDropPos)
+    for _, p := range Players() {
+        u, err := data.LoadUserFromName(p.Name())
+        if err != nil {
+            continue
+        }
+        h, ok := p.Handler().(userHandler)
+        if !ok {
+            continue
+        }
+        u.PlayTime += h.LogTime()
+        data.SaveUser(u)
 
-		p.Disconnect("Server is shutting down.")
-	}
+        p.Disconnect("Server is shutting down.")
+    }
 
-	time.Sleep(time.Millisecond * 500)
+    time.Sleep(time.Millisecond * 500)
 
-	sotw.Save()
-	_ = Nether().Close()
+    sotw.Save()
+    _ = Nether().Close()
     _ = End().Close()
     _ = srv.World().Close()
-	if err := srv.Close(); err != nil {
-		logrus.Fatalln("close server: %v", err)
-	}
+    if err := srv.Close(); err != nil {
+        logrus.Fatalln("close server: %v", err)
+    }
 }
 
 func tickAutomaticSave(w *world.World) {
-	for {
-		<-time.After(time.Minute * 1)
-		w.Save()
-		for _, p := range Players() {
-			u, err := data.LoadUserFromName(p.Name())
-			if err != nil {
-				fmt.Println("load user: ", err)
-				continue
-			}
-			if u.StaffMode {
-				continue
-			}
+    for {
+        <-time.After(time.Minute * 1)
+        w.Save()
+        for _, p := range Players() {
+            u, err := data.LoadUserFromName(p.Name())
+            if err != nil {
+                fmt.Println("load user: ", err)
+                continue
+            }
+            if u.StaffMode {
+                continue
+            }
 
-			err = playerProvider.SavePlayer(p)
-			if err != nil {
-				fmt.Printf("save player: %v\n", err)
-			}
-		}
-	}
+            err = playerProvider.SavePlayer(p)
+            if err != nil {
+                fmt.Printf("save player: %v\n", err)
+            }
+        }
+    }
 }
 
 type userHandler interface {
-	player.Handler
-	LogTime() time.Duration
+    player.Handler
+    LogTime() time.Duration
 }
