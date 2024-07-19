e283f459503e57f904621cabf7fbef9a8466b30a
Restart <45609733+RestartFU@users.noreply.github.com>
2024-07-18 20:01:06 -0400

    various changes

--- moyai/user/handler_command_execution.go
+++ moyai/user/handler_command_execution.go
@@ -1,60 +1,78 @@
 package user
 
 import (
-	"strings"
+    "fmt"
+    rls "github.com/moyai-network/teams/moyai/roles"
+    "slices"
+    "strings"
+    "time"
 
-	"github.com/df-mc/dragonfly/server/cmd"
-	"github.com/df-mc/dragonfly/server/event"
-	"github.com/moyai-network/teams/moyai"
-	"github.com/moyai-network/teams/moyai/data"
+    "github.com/df-mc/dragonfly/server/cmd"
+    "github.com/df-mc/dragonfly/server/event"
+    "github.com/moyai-network/teams/moyai"
+    "github.com/moyai-network/teams/moyai/data"
 )
 
 var (
-	spawn  = []string{}
-	combat = []string{
-		"ec",
-		"logout",
-		"pv",
-	}
-	deathban = []string{
-		"reclaim",
-		"trim",
-		"prizes",
-		"logout",
-		"ec",
-	}
+    spawn  = []string{}
+    combat = []string{
+        "ec",
+        "logout",
+        "pv",
+    }
+    deathban = []string{
+        "reclaim",
+        "trim",
+        "prizes",
+        "logout",
+        "ec",
+    }
 )
 
 func (h *Handler) HandleCommandExecution(ctx *event.Context, command cmd.Command, args []string) {
-	u, err := data.LoadUserFromName(h.p.Name())
-	if err != nil {
-		return
-	}
-	names := append(command.Aliases(), command.Name())
+    u, err := data.LoadUserFromName(h.p.Name())
+    if err != nil {
+        return
+    }
+    names := append(command.Aliases(), command.Name())
 
-	if h.tagCombat.Active() && containsAny(combat, names...) {
-		moyai.Messagef(h.p, "command.error.combat-tagged")
-		ctx.Cancel()
-	} else if u.Teams.DeathBan.Active() && containsAny(deathban, names...) {
-		moyai.Messagef(h.p, "command.error.death-ban")
-		ctx.Cancel()
-	}
+    if time.Since(h.lastMessage.Load()) < moyai.ChatCoolDown() && !u.Roles.Contains(rls.Admin()) {
+        fmt.Println(names)
+        whisper := slices.Contains(names, "whisper")
+        reply := slices.Contains(names, "reply")
+        if whisper || reply {
+            ctx.Cancel()
+            moyai.Messagef(h.p, "chat.cooldown", time.Until(h.lastMessage.Load().Add(moyai.ChatCoolDown())).Seconds())
+            return
+        }
+        return
+    } else {
+        h.lastMessage.Store(time.Now().Add(moyai.ChatCoolDown()))
+    }
+
+    if h.tagCombat.Active() && containsAny(combat, names...) {
+        moyai.Messagef(h.p, "command.error.combat-tagged")
+        ctx.Cancel()
+    } else if u.Teams.DeathBan.Active() && containsAny(deathban, names...) {
+        moyai.Messagef(h.p, "command.error.death-ban")
+        ctx.Cancel()
+    }
 }
 
 func containsAny(s []string, e ...string) bool {
-	for _, a := range e {
-		if contains(s, a) {
-			return true
-		}
-	}
-	return false
+    for _, a := range e {
+        if contains(s, a) {
+            return true
+        }
+    }
+    return false
 }
 
 func contains(s []string, e string) bool {
-	for _, a := range s {
-		if strings.EqualFold(a, e) {
-			return true
-		}
-	}
-	return false
+    for _, a := range s {
+        if strings.EqualFold(a, e) {
+            return true
+        }
+    }
+    return false
 }
