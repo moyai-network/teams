9ee1a5a29bc4047e19d3db7e6b8251a8b8ebf5c3
Restart <45609733+RestartFU@users.noreply.github.com>
2024-07-19 12:40:42 -0400

    Update staff.go

--- moyai/command/staff.go
+++ moyai/command/staff.go
@@ -1,77 +1,79 @@
 package command
 
 import (
-	"github.com/df-mc/dragonfly/server/cmd"
-	"github.com/df-mc/dragonfly/server/item/inventory"
-	"github.com/df-mc/dragonfly/server/player"
-	"github.com/df-mc/dragonfly/server/world"
-	"github.com/moyai-network/teams/moyai"
-	"github.com/moyai-network/teams/moyai/data"
-	"github.com/moyai-network/teams/moyai/kit"
-	"github.com/moyai-network/teams/moyai/user"
+    "github.com/df-mc/dragonfly/server/cmd"
+    "github.com/df-mc/dragonfly/server/item/inventory"
+    "github.com/df-mc/dragonfly/server/player"
+    "github.com/df-mc/dragonfly/server/world"
+    "github.com/moyai-network/teams/moyai"
+    "github.com/moyai-network/teams/moyai/data"
+    "github.com/moyai-network/teams/moyai/kit"
+    "github.com/moyai-network/teams/moyai/user"
 )
 
 type StaffMode struct {
-	trialAllower
-	Sub cmd.SubCommand `cmd:"mode"`
+    trialAllower
+    Sub cmd.SubCommand `cmd:"mode"`
 }
 
 func (StaffMode) Run(s cmd.Source, o *cmd.Output) {
-	p, ok := s.(*player.Player)
-	if !ok {
-		return
-	}
+    p, ok := s.(*player.Player)
+    if !ok {
+        return
+    }
 
-	u, err := data.LoadUserFromName(p.Name())
-	if err != nil {
-		return
-	}
-	mode := p.GameMode()
-	u.StaffMode = !u.StaffMode
+    u, err := data.LoadUserFromName(p.Name())
+    if err != nil {
+        return
+    }
+    mode := p.GameMode()
+    u.StaffMode = !u.StaffMode
 
-	if u.StaffMode {
-		_ = moyai.PlayerProvider().SavePlayer(p)
-		if !u.Vanished {
-			p.SetGameMode(vanishGameMode{lastMode: mode})
-			moyai.Messagef(p, "command.vanish.enabled")
-			u.Vanished = true
-		}
+    if u.StaffMode {
+        _ = moyai.PlayerProvider().SavePlayer(p)
+        if !u.Vanished {
+            p.SetGameMode(vanishGameMode{lastMode: mode})
+            moyai.Messagef(p, "command.vanish.enabled")
+            u.Vanished = true
+        }
 
-		p.Inventory().Clear()
-		p.Armour().Clear()
-		kit.Apply(kit.Staff{}, p)
-		p.Inventory().Handle(user.StaffInventoryHandler{})
-	} else {
-		if u.Vanished {
-			vanishMode, ok := mode.(vanishGameMode)
-			if !ok {
-				p.SetGameMode(world.GameModeCreative)
-			} else {
-				p.SetGameMode(vanishMode.lastMode)
-			}
-			moyai.Messagef(p, "command.vanish.disabled")
-		}
+        p.Inventory().Clear()
+        p.Armour().Clear()
+        kit.Apply(kit.Staff{}, p)
+        p.Inventory().Handle(user.StaffInventoryHandler{})
+    } else {
+        if u.Vanished {
+            u.Vanished = false
 
-		p.Inventory().Handle(inventory.NopHandler{})
-		dat, err := moyai.LoadPlayerData(p.UUID())
-		if err != nil {
-			return
-		}
-		p.Teleport(dat.Position)
-		p.SetGameMode(dat.GameMode)
+            vanishMode, ok := mode.(vanishGameMode)
+            if !ok {
+                p.SetGameMode(world.GameModeCreative)
+            } else {
+                p.SetGameMode(vanishMode.lastMode)
+            }
+            moyai.Messagef(p, "command.vanish.disabled")
+        }
 
-		newInv := dat.Inventory
-		p.Inventory().Clear()
-		p.Armour().Clear()
-		p.Armour().Set(newInv.Helmet, newInv.Chestplate, newInv.Leggings, newInv.Boots)
-		for slot, it := range newInv.Items {
-			if it.Empty() {
-				continue
-			}
-			_ = p.Inventory().SetItem(slot, it)
-		}
-	}
+        p.Inventory().Handle(inventory.NopHandler{})
+        dat, err := moyai.LoadPlayerData(p.UUID())
+        if err != nil {
+            return
+        }
+        p.Teleport(dat.Position)
+        p.SetGameMode(dat.GameMode)
 
-	data.SaveUser(u)
-	user.UpdateVanishState(p, u)
+        newInv := dat.Inventory
+        p.Inventory().Clear()
+        p.Armour().Clear()
+        p.Armour().Set(newInv.Helmet, newInv.Chestplate, newInv.Leggings, newInv.Boots)
+        for slot, it := range newInv.Items {
+            if it.Empty() {
+                continue
+            }
+            _ = p.Inventory().SetItem(slot, it)
+        }
+    }
+
+    data.SaveUser(u)
+    user.UpdateVanishState(p, u)
 }
